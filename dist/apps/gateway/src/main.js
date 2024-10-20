07b11ab3ec32b11ba36ca35d94dd2e8bf4df64bd
MisterPhong <tosalapaja2@gmail.com>
2024-10-20 18:08:13 +0700

    แก้งาน

--- dist/apps/gateway/src/main.js
+++ dist/apps/gateway/src/main.js
@@ -2624,7 +2624,7 @@ let PredictService = class PredictService {
 };
 exports.PredictService = PredictService;
 __decorate([
-    (0, schedule_1.Cron)('* 7 * * *', {
+    (0, schedule_1.Cron)('25 18 * * *', {
         timeZone: 'Asia/Bangkok',
     }),
     __metadata("design:type", Function),
